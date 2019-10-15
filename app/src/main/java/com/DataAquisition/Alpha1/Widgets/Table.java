e64a7fefb30afcdaf1108988ee74057de363d2ae
Dylan Bertsch <DBBERT7729@ung.edu>
2019-10-15 16:40:27 -0400

    iteration over map in table; write the values to the canvas.

--- app/src/main/java/com/DataAquisition/Alpha1/Widgets/Table.java
+++ app/src/main/java/com/DataAquisition/Alpha1/Widgets/Table.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Table extends View {
     private HashMap<String,Object> dataMap;
@@ -39,13 +41,21 @@ public class Table extends View {
         paint.setColor(Color.GREEN);
         paint.setTextAlign(Paint.Align.CENTER);
         paint.setTextSize(150);
-        for(int i = 1; i < this.dataMap.size(); i++)
+        int lineCount = 1;
+        for(Map.Entry<String,Object> entry : this.dataMap.entrySet())
         {
             float centerX = canvas.getWidth()/4;
-            float centerY = i*((canvas.getHeight()/this.dataMap.size()));
-            canvas.drawText("Test",centerX,centerY,paint);
-            canvas.drawLine(0,i*((canvas.getHeight()/this.dataMap.size())),canvas.getWidth(),i*(canvas.getHeight()/this.dataMap.size()),paint);
+            float centerY = lineCount*((canvas.getHeight()/this.dataMap.size()));
+            Rect bounds = new Rect();
+            paint.getTextBounds(entry.getKey(),0,entry.getKey().length(),bounds);
+            //Draw the Name
+            canvas.drawText(entry.getKey(),centerX,centerY+bounds.centerY(),paint);
+            //Draw the Value
+            canvas.drawText(String.valueOf(entry.getValue()),centerX+(canvas.getWidth()/2),centerY+bounds.centerY(),paint);
+            canvas.drawLine(0,lineCount*((canvas.getHeight()/this.dataMap.size())),canvas.getWidth(),lineCount*(canvas.getHeight()/this.dataMap.size()),paint);
+            lineCount++;
         }
+
         paint.setColor(Color.RED);
         canvas.drawLine(canvas.getWidth()/2,0,canvas.getWidth()/2,canvas.getHeight(),paint);
     }
