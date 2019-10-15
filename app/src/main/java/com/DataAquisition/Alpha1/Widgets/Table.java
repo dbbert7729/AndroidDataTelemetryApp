6c2b23d6633b1ada45fb68756ffc763d4e877799
Dylan Bertsch <DBBERT7729@ung.edu>
2019-10-14 21:37:37 -0400

    Added more features and working on Table widget

---
+++ app/src/main/java/com/DataAquisition/Alpha1/Widgets/Table.java
@@ -0,0 +1,10 @@
+package com.DataAquisition.Alpha1.Widgets;
+
+import android.content.Context;
+import android.view.View;
+
+public class Table extends View {
+    public Table(Context context) {
+        super(context);
+    }
+}
